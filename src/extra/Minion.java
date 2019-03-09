afa261eeec0f880d59e17d3ac398ddfedb1312e2
ilabstaff <ilabstaff@172.16.116.73>
2019-03-09 10:50:35 -0800

    MinionTest

--- src/extra/Minion.java
+++ src/extra/Minion.java
@@ -1,11 +1,32 @@
 package extra;
 
 public class Minion {
-
-	Minion dave = new Minion();
 	private String name;
 	private int eyes;
 	private String color;
 	private String master;
+	Minion (String name, int eyes, String color){
+		this.name= name;
+		this.eyes = eyes;
+		this.color= color;	
+	}
+	String getColor() {
+		return this.color;
+	}
+	void setEyes(int eyes){
+		if(eyes>0) {
+			this.eyes=eyes;
+		}
+	}
+	void setName(String name) {
+		if(name.equals("dave")) {
+			this.name=name;
+		}
+		if(name.equals("stuart")) {
+			this.name=name;
+		}
+	}
 	
 }
+	
+
